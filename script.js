dc3fa01624827c89ed2b5e286f520ab7566c7875
CFullum72 <cfullum@kent.edu>
2022-04-26 00:02:36 -0400

    Two operands will make op act as enter

--- script.js
+++ script.js
@@ -2,7 +2,37 @@ const keys = document.querySelector('.keys')
 let previousOperand = document.getElementById('previous-operand');
 let prevNum;
 let currentOperand = document.getElementById('current-operand')
-let operator;
+let operator ;
+
+//can use keyboard
+document.addEventListener('keydown', key => {
+    //0-9
+    if(0 <= key.key && key.key <= 9){
+        appendNumbers(key.key);
+    }
+    //+, -, *, /
+    else if(key.key == '+' || key.key == '-' || key.key == '*' || key.key == '/'){
+        handleOperator(key.key);
+    }
+    else if(key.key == 'ArrowUp'){
+        handleOperator('^')
+    }
+    //enter or return
+    else if( key.key == 'Enter') {
+        onEnter();
+    }
+    //backspace
+    else if(key.key == 'Backspace') clear();
+    /*
+    switch(key.key){
+    case '+':
+        handleOperator('+');
+        break;
+    case '-':
+        handleOperator('-')
+    }
+    */
+});
 
 keys.addEventListener('click', key => {
     element = key.target;
@@ -10,10 +40,8 @@ keys.addEventListener('click', key => {
         //if clear, then clear text
         if(element.value == "clear") clear();        
         //if equal, calculate
-        else if(element.value == "=" && isReady()){
-            let num = calculate(prevNum, parseFloat(currentOperand.innerText), operator);
-            clear();
-            currentOperand.innerText = num;
+        else if(element.value == "="){
+            onEnter();
         } 
         //if operator, set operator, appendOperator, move to previous operator
         else if(element.value == '+' || element.value == '-' || element.value == '*' || 
@@ -21,21 +49,27 @@ keys.addEventListener('click', key => {
                 handleOperator(element.value);
 
         //otherwise just append the numbers
-        else if(element.value != "=")appendNumbers(element.value)
+        else appendNumbers(element.value)
     }
 })
 
 function isReady(){
-    return previousOperand.innerText == '' || currentOperand != '' || operator != '';
+    //returns true if prevtext is not empty, currentOperand is not empty, and operator is not empty
+    return previousOperand.innerText != '' && currentOperand.innerText != '' && operator != '';
 }
 
 function handleOperator(op) {
-
+    if(currentOperand.innerText){
         operator = op;
+        if(isReady()){
+            let num = calculate(prevNum, parseFloat(currentOperand.innerText), operator);
+            currentOperand.innerHTML = num
+        }
         prevNum = parseFloat(currentOperand.innerText);
         appendNumbers(op);
         previousOperand.innerText = currentOperand.innerText;
         currentOperand.innerText = '';
+    }
 }
 
 function appendNumbers(digit){
@@ -66,7 +100,15 @@ function calculate(operand1, operand2, operator) {
             return Math.pow(operand1, operand2);
             break;
         default:
-            console.log(`Error choosing operator: ${operator}`);
+            console.log(`Error choosing operator: ${operator} ..`);
             break;
+    }
+}
+
+function onEnter(){
+    if(isReady()){
+        let num = calculate(prevNum, parseFloat(currentOperand.innerText), operator);
+        clear();
+        currentOperand.innerText = num;
     }
 }
